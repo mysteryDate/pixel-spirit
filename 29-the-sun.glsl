f52f09c0488c33fe95100a1e8934ea0375664a0c
Aaron Krajeski <ak@aaronk.is>
2017-11-25 12:48:50 -0500

    Finish up the devil, the sun might be too tough

--- 29-the-sun.glsl
+++ 29-the-sun.glsl
@@ -93,7 +93,7 @@ vec2 rotate(vec2 st, float a) {
   return rotated + 0.5;
 }
 
-const int numPoints = 5;
+const int numPoints = 8;
 const float starSize = 0.5;
 const float strokeWidth = 0.05;
 const float circleSize = 0.8;
@@ -101,23 +101,9 @@ void main() {
   vec3 color = vec3(0.0);
   vec2 st = gl_FragCoord.xy / iResolution.xy;
 
-  // My attempt, hacky but close I think
-  // st = rotate(st, radians(-15.0));
-  // // float star = starSDF(st, numPoints, sin(5.0 * u_time / PI + PI/4.0)/10.0);
-  // float star = starSDF(st, numPoints, 0.1);
-  // color += stroke(star, starSize, strokeWidth);
-  // float circle = circleSDF(st);
-  // // color += stroke(circle, starSize * 1.8 * mod(u_time, 2.0) / 2.0, strokeWidth/2.0);
-  // color += stroke(circle, starSize * 1.8, strokeWidth/2.0);
-  // color -= stroke(star, starSize + strokeWidth, strokeWidth);
-  // color -= stroke(star, starSize + 2.0 * strokeWidth, strokeWidth);
-
-  // Their code, magic numbers and all
-  color += stroke(circleSDF(st), circleSize, strokeWidth);
-  st.y = 1.0 - st.y;
-  float star = starSDF(st.yx, numPoints, 0.1);
-  color *= step(0.7, star);
-  color += stroke(star, 0.4, 0.1);
+  // My code, 0.1 is curious here
+  float star = starSDF(st, numPoints, 0.1);
+  color += fill(star, starSize);
 
   gl_FragColor = vec4(color, 1.0);
 }
