365d428b09fee4c243e41c574f2c6dff95b5652d
Aaron Krajeski <ak@aaronk.is>
2018-03-26 12:15:53 +0100

    The magician

--- 35-the-magician.glsl
+++ 35-the-magician.glsl
@@ -1,12 +1,30 @@
 #pragma glslify: circleSDF = require('./lib/sdf/circleSDF')
-#pragma glslify: fill = require('./lib/drawing/fill')
+#pragma glslify: stroke = require('./lib/drawing/stroke')
+#pragma glslify: bridge = require('./lib/drawing/bridge')
+#pragma glslify: flip = require('./lib/drawing/flip')
 #pragma glslify: map = require('./lib/map')
 
+const vec2 offset = vec2(0.15, 0.0);
+const float radius = 0.4;
+const float width = 0.075;
 void main() {
   vec3 color = vec3(0.0);
   vec2 st = gl_FragCoord.xy / iResolution.xy;
 
-  float d = map(sin(u_time), -1.0, 1.0, 0.0, 0.2);
-  color += fill(circleSDF(st + vec2(d, 0.0)), 0.4);
+  // My solution (I looked up flip)
+  // st.x = flip(st.x, step(0.5, st.y));
+  // float leftCircle = circleSDF(st + offset);
+  // color += stroke(leftCircle, radius, width);
+  // float rightCircle = circleSDF(st - offset);
+  // color += stroke(rightCircle, radius, width);
+  // color = bridge(color, rightCircle, radius, width);
+
+  // Their solution, slightly more elegant
+  st.x = flip(st.x, step(0.5, st.y));
+  float left = circleSDF(st + offset);
+  float right = circleSDF(st - offset);
+  color += stroke(left, radius, width);
+  color = bridge(color, right, radius, width);
+
   gl_FragColor = vec4(color, 1.0);
 }
