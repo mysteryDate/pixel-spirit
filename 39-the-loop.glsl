0429df81f69fc568196768da5464504615ec473f
Aaron Krajeski <ak@aaronk.is>
2018-03-29 14:38:05 +0200

    Finish the loop

--- 39-the-loop.glsl
+++ 39-the-loop.glsl
@@ -5,42 +5,52 @@
 #pragma glslify: flip = require('./lib/drawing/flip')
 #pragma glslify: map = require('./lib/map')
 
-const float size = 0.3;
-const float strokeWidth = 0.04;
-// const float medOffset = 0.2;
-// const float smallOffset = 0.1;
+const vec2 CENTER = vec2(0.5);
+const float size = 0.25;
+const float strokeWidth = 0.03;
+const float medSize = size / 1.5;
+const float medOffset = size / 1.8;
+const float smallSize = medSize / 4.0;
+const float smallOffset = medOffset + medSize * sqrt(2.0)/2.0 + sqrt(2.0)/2.0 * smallSize;
+const float angle = radians(45.0);
 void main() {
   vec3 color = vec3(0.0);
   vec2 st = gl_FragCoord.xy / iResolution.xy;
 
-  st = rotateAboutPoint(st, radians(180.0) * step(0.5, st.x), vec2(0.5));
-
-  float angle = radians(45.0);
-  float square = rectangleSDF(st, vec2(1.0));
-  vec2 rotatedST = rotateAboutPoint(st, angle, vec2(0.5));
-  float bigSquare = rectangleSDF(rotatedST, vec2(1.0));
-
-  float bounceTimer = map(sin(u_time/2.0), -1.0, 1.0, 0.0, 1.0);
-  float medSquareSize = size * 0.5;
-
-  color *= 0.0;
+  // My solution, long, but I like it
+  st = rotateAboutPoint(st, radians(180.0) * step(0.5, st.x), CENTER);
+  vec2 bigSquareST = rotateAboutPoint(st, angle, CENTER);
+  float bigSquare = rectangleSDF(bigSquareST);
   color += stroke(bigSquare, size, strokeWidth);
-  vec2 medSquareST = rotateAboutPoint(st + vec2(0.2, 0.0), angle, vec2(0.5));
-  float mediumSquare = rectangleSDF(medSquareST, vec2(1.0));
-  color += stroke(mediumSquare, medSquareSize, strokeWidth);
 
+  vec2 medSquareST = rotateAboutPoint(st + vec2(medOffset, 0.0), angle, CENTER);
+  float mediumSquare = rectangleSDF(medSquareST);
+  color += stroke(mediumSquare, medSize, strokeWidth);
   float invertControl = step(0.5, st.y);
   float bridges = mix(bigSquare, mediumSquare, invertControl);
-  float bridgeSize = mix(size, medSquareSize, invertControl);
+  float bridgeSize = mix(size, medSize, invertControl);
   color = bridge(color, bridges, bridgeSize, strokeWidth);
 
-  float smallSquareSize = medSquareSize / 4.0;
-  vec2 smallSquareST = rotateAboutPoint(st + vec2(0.333, 0.0), angle, vec2(0.5));
-  float smallSquare = rectangleSDF(smallSquareST, vec2(1.0));
-  color += stroke(smallSquare, smallSquareSize, strokeWidth);
+  vec2 smallSquareST = rotateAboutPoint(st + vec2(smallOffset, 0.0), angle, CENTER);
+  float smallSquare = rectangleSDF(smallSquareST);
+  color += stroke(smallSquare, smallSize, strokeWidth);
   bridges = mix(mediumSquare, smallSquare, 1.0 - invertControl);
-  bridgeSize = mix(medSquareSize, smallSquareSize, 1.0 - invertControl);
+  bridgeSize = mix(medSize, smallSize, 1.0 - invertControl);
   color = bridge(color, bridges, bridgeSize, strokeWidth);
+
+  // Their solution, senseless, yet cool
+  color *= 0.0;
+  st = gl_FragCoord.xy / iResolution.xy;
+  float inv = step(0.5, st.y);
+  st = rotateAboutPoint(st, -angle, CENTER) - 0.2;
+  st = mix(st, 0.6 - st, step(0.5, inv));
+  for (int i = 0; i < 5; i++) {
+    float currentRect = rectangleSDF(st);
+    float currentSize = size;
+    currentSize -= abs(0.1 * float(i) - 0.2);
+    color = bridge(color, currentRect, currentSize, strokeWidth);
+    st += 0.1;
+  }
 
   gl_FragColor = vec4(color, 1.0);
 }
--- lib/sdf/rectangleSDF.glsl
+++ lib/sdf/rectangleSDF.glsl
@@ -3,4 +3,8 @@ float rectangleSDF(vec2 st, vec2 s) {
   return max(abs(st.x/s.x), abs(st.y/s.y));
 }
 
+float rectangleSDF(vec2 st) { // For squares
+  return rectangleSDF(st, vec2(1.0));
+}
+
 #pragma glslify: export(rectangleSDF)
