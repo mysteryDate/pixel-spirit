528a335486ee92b08b4c1c028a19fb9c58fac8ac
Aaron Krajeski <ak@aaronk.is>
2018-04-01 18:50:45 +0200

    Oops, it's actually called the elders

--- 43-the-elders.glsl
+++ 43-the-elders.glsl
@@ -8,35 +8,41 @@ const vec2 CENTER = vec2(0.5);
 const float size = 0.45;
 const float strokeWidth = 0.05;
 const float vesicaSlope = 0.3;
-const vec2 offset = vec2(0.0, 0.189);
-const int NUM_POINTS = 12;
+const vec2 offset = vec2(0.0, 0.09);
+const int NUM_POINTS = 3;
 
 vec3 mySolution(vec2 st) {
   vec3 color = vec3(0.0);
 
   float angle = 2.0 * PI / float(NUM_POINTS);
   for(int i = 0; i < NUM_POINTS; i++) {
+    vec2 nextST = rotateAboutPoint(st, angle, CENTER);
     float center = vesicaSDF(st - offset, vesicaSlope);
-    float distanceMask = step(0.9 * size/2.0, length(st - CENTER));
-    color = bridge(color, center * distanceMask, size, strokeWidth);
-    color += stroke(center * (1.0 - distanceMask), size, strokeWidth);
-    st = rotateAboutPoint(st, angle, CENTER);
+    float next = vesicaSDF(nextST - offset, vesicaSlope);
+    color += stroke(next, size, strokeWidth) * step(0.5, nextST.x);
+    float bridgeControl = (1.0 - step(0.5, st.x)) * step(0.5, st.y);
+    color = bridge(color, center * bridgeControl, size, strokeWidth);
+    st = nextST;
   }
 
   return color;
 }
 
-vec3 theirSolution(vec2 st) {
+vec3 theirSolution(vec2 st) { // respectfully, this is a fucking mess
   vec3 color = vec3(0.0);
 
   float angle = 2.0 * PI / float(NUM_POINTS);
-  for(int i = 0; i < NUM_POINTS; i++) {
+  for(int i = 0; i < 2 * NUM_POINTS; i++) {
     vec2 xy = rotateAboutPoint(st, angle * float(i), CENTER) - offset;
     float vsc = vesicaSDF(xy, vesicaSlope);
-    color *= 1.0 - stroke(vsc, size, 0.1) * step(0.5, xy.y);
-    color += stroke(vsc, size, strokeWidth);
+    color = mix(
+      color + stroke(vsc, size, strokeWidth),
+      mix(
+        color,
+        bridge(color, vsc, size, strokeWidth),
+      step(xy.x, 0.5) - step(xy.y, 0.4)),
+    step(3.0, float(i)));
   }
-
   return color;
 }
 
