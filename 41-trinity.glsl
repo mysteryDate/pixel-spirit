7d730d0ddb5a24904398e9c313b183bb77e2130d
Aaron Krajeski <ak@aaronk.is>
2018-04-01 17:50:28 +0200

    Their solution is clean, but I don't like it.

--- 41-trinity.glsl
+++ 41-trinity.glsl
@@ -6,8 +6,8 @@
 
 #define PI 3.14159
 const vec2 CENTER = vec2(0.5);
-const float size = 0.2;
-const float strokeWidth = 0.04;
+const float size = 0.25;
+const float strokeWidth = 0.08;
 const vec2 offset = vec2(0.0, 0.1);
 
 vec3 mySolution(vec2 st) { // A bit hacky, but it works!
@@ -27,9 +27,24 @@ vec3 mySolution(vec2 st) { // A bit hacky, but it works!
   return color;
 }
 
-vec3 theirSolution(vec2 st) {
+vec3 theirSolution(vec2 st) { // I actually think this is quite clean
   vec3 color = vec3(0.0);
 
+  st.y = 1.0 - st.y;
+  float t1 = polygonSDF(st + vec2(0.0, 0.175), 3);
+  float t2 = polygonSDF(st + vec2(0.1, 0.0), 3);
+  float t3 = polygonSDF(st - vec2(0.1, 0.0), 3);
+  color += stroke(t1, size, strokeWidth);
+  color += stroke(t2, size, strokeWidth);
+  color += stroke(t3, size, strokeWidth);
+
+  // take advantage of the quadrant-ness similar to my mySolution
+  float bridges = mix(
+                    mix(t1, t2, step(0.5, st.y)),
+                    mix(t3, t2, step(0.5, st.y)),
+                  step(0.5, st.x));
+
+  color = bridge(color, bridges, size, strokeWidth);
   return color;
 }
 
@@ -37,8 +52,11 @@ void main() {
   vec3 color = vec3(0.0);
   vec2 st = gl_FragCoord.xy / iResolution.xy;
 
-  color = mySolution(st);
-  // color = theirSolution(st); // They made theirs huge..
+  color += vec3(0.0, 1.0, 0.0) * mySolution(st);
+  color += vec3(1.0, 0.0, 0.0) * theirSolution(st);
+
+  // Their solution isn't even in the center!
+  color += vec3(1.0) * (1.0 - step(0.05, length(st - CENTER)));
 
   gl_FragColor = vec4(color, 1.0);
 }
