1af2db5f8b68c23c741c55e4966ff8ed2ddd6826
Aaron Krajeski <ak@aaronk.is>
2018-04-02 16:42:53 +0100

    Rainbow squaredance animation

--- 44-the-core.glsl
+++ 44-the-core.glsl
@@ -3,17 +3,18 @@
 #pragma glslify: stroke = require('./lib/drawing/stroke')
 #pragma glslify: fill = require('./lib/drawing/fill')
 #pragma glslify: bridge = require('./lib/drawing/bridge')
+#pragma glslify: hsv2rgb = require('./lib/hsv2rgb')
 #pragma glslify: map = require('./lib/map')
 
 #define PI 3.14159
 const vec2 CENTER = vec2(0.5);
-const float size = 0.116;
-const float strokeWidth = 0.003;
-const vec2 offset = vec2(0.0, 0.2);
+const float size = 0.176;
+const float strokeWidth = 0.007;
+const vec2 offset = vec2(0.0, 0.3);
 // const int NUM_POINTS = 8;
 
 const float duration = 50.0;
-const float squareBrightness = 1.0 / 4.0;
+const float squareBrightness = 1.0 / 2.0;
 #define numSquares 36.0
 vec3 mySolution(vec2 st) {
   vec3 color = vec3(0.0);
@@ -26,13 +27,17 @@ vec3 mySolution(vec2 st) {
   for(float i = 0.0; i < numSquares; i++) {
     vec2 xy = rotateAboutPoint(st, angle * i, CENTER);
     float square = rectangleSDF(rotateAboutPoint(xy - offset, PI/4.0, CENTER));
-    color += fill(square, size) * squareBrightness;
+    // vec3 squareColor = vec3(0.33 * mod(i, 3.0), 0.33 * mod(i + 1.0, 3.0), 0.33 * mod(i + 2.0, 3.0));
+    float hue = i / numSquares;
+    vec3 squareColor = hsv2rgb(vec3(hue, 1.0, 1.0));
+    color += fill(square, size) * squareBrightness * squareColor;
 
     float centerLine = stroke(abs(xy.x - 0.5), 0.0, strokeWidth);
     float lineLength = 2.0;
     // float lineLength = max(1.19, length(st.y - xy.y));
     // lineLength = 1.0 - step(0.2, lineLength);
-    color += centerLine * step(lineLength, 1.0/length(st.y - xy.y)) / 3.0;
+    centerLine *= step(lineLength, 1.0/length(st.y - xy.y)) / 3.0;
+    color += centerLine * squareColor;
   }
 
   return color;
@@ -50,6 +55,10 @@ void main() {
 
   color += mySolution(st);
   // color += theirSolution(st);
+  // color = 1.0 - color;
+  vec3 invColor = 1.0 - color;
+  // color = mix(color, invColor, sin(2.0 * PI * u_time));
+  // color = invColor;
 
   gl_FragColor = vec4(color, 1.0);
 }
